ce9b24380ceee068aacb54ea1392891cd6f4412e
pkg-dot-zip <git_sign_mail_.37np7@4wrd.cc>
2025-10-08 12:22:11 +0200

    MPOMerger now checks if dirs exist

--- src/util/mpo_merger.py
+++ src/util/mpo_merger.py
@@ -10,8 +10,10 @@ class MPOMerger:
     def merge_folders(self, export_dir: Path, left_dir: Path, right_dir: Path):
         """Assumes left and right dir children file names are equal. Will skip entries that are not in both."""
 
+        if not left_dir.exists() or not right_dir.exists():
+            return
+
         # TODO: Check if image is actually image.
-        # TODO: Check if all dirs exist.
         for image in left_dir.iterdir():
             left_image = image
             right_image = right_dir / image.name
@@ -22,6 +24,10 @@ class MPOMerger:
     def merge(self, export_path: Path, img_path_1: Path, img_path_2: Path):
         """Merges two images into a single .mpo image."""
         # TODO: Check if image is actually image.
+
+        if not img_path_1.exists() or not img_path_2.exists():
+            return
+
         from PIL import Image
         im1 = Image.open(img_path_1)
         im2 = Image.open(img_path_2)
